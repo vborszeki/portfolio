614da746f203045c02efa549784bd4f3e9678950
Viktor Borszeki <vborszeki@gmail.com>
2018-03-22 18:52:59 +0100

    add skeleton for mobile layout

--- src/App/App.js
+++ src/App/App.js
@@ -9,9 +9,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      language: 'hu'
+      language: 'hu',
+      width: window.innerWidth
     };
   }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
 
   toggleLanguage() {
     const language = this.state.language === 'hu' ? 'en' : 'hu';
@@ -20,40 +33,52 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" render={() => <StartPage />} />
-          <Route
-            exact
-            path="/bio"
-            render={({ match }) => (
-              <Bio
-                toggleLanguage={() => this.toggleLanguage()}
-                language={this.state.language}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/:category"
-            render={({ match }) => <Works category={match.params.category} />}
-          />
-          <Route
-            exact
-            path="/:category/:projectTitle"
-            render={({ match }) => (
-              <Project
-                category={match.params.category}
-                projectTitle={match.params.projectTitle}
-                toggleLanguage={() => this.toggleLanguage()}
-                language={this.state.language}
-              />
-            )}
-          />
-        </Switch>
-      </Router>
-    );
+    const isMobile = this.state.width <= 500;
+
+    if (isMobile) {
+      return (
+        <h1>
+          <span role="img" aria-label="mobile phone">
+            📱
+          </span>
+        </h1>
+      );
+    } else {
+      return (
+        <Router>
+          <Switch>
+            <Route exact path="/" render={() => <StartPage />} />
+            <Route
+              exact
+              path="/bio"
+              render={({ match }) => (
+                <Bio
+                  toggleLanguage={() => this.toggleLanguage()}
+                  language={this.state.language}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/:category"
+              render={({ match }) => <Works category={match.params.category} />}
+            />
+            <Route
+              exact
+              path="/:category/:projectTitle"
+              render={({ match }) => (
+                <Project
+                  category={match.params.category}
+                  projectTitle={match.params.projectTitle}
+                  toggleLanguage={() => this.toggleLanguage()}
+                  language={this.state.language}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      );
+    }
   }
 }
 
