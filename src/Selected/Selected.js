ecc57d702c7fca1c80e9c2e5f27a024faf1e854c
Viktor Borszeki <vborszeki@gmail.com>
2018-07-18 22:08:54 +0200

    fix style of svg

--- src/Selected/Selected.js
+++ src/Selected/Selected.js
@@ -4,7 +4,6 @@ import './selected.css';
 const Selected = ({ height }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" height={height}>
     <path className="selected-x" d="M 0,0 L 10,10 M 10,0 L 0,10" />
-    <path className="selected-border" d="M 0,0 L 0,10" />
   </svg>
 );
 
