262bf6b39ecec3df507c7acc73d54fcea5703ccf
Viktor Borszeki <vborszeki@gmail.com>
2019-02-04 23:37:15 +0100

    render project stepper conditionally

--- src/Project/ProjectPager.js
+++ src/Project/ProjectPager.js
@@ -5,15 +5,19 @@ import './projectPager.css';
 
 const ProjectPager = ({ counter, height, category, project }) => (
   <div className="project-pager">
-    <div className="project-pager--box" style={{ width: height }}>
-      <Link to={`/${category}/${project.previousProject}`}>PREV</Link>
-    </div>
-    <div className="project-pager--box" style={{ width: height }}>
-      {counter}
-    </div>
-    <div className="project-pager--box" style={{ width: height }}>
-      <Link to={`/${category}/${project.nextProject}`}>NEXT</Link>
-    </div>
+    {project.nextProject && (
+      <>
+        <div className="project-pager--box" style={{ width: height }}>
+          <Link to={`/${category}/${project.previousProject}`}>PREV</Link>
+        </div>
+        <div className="project-pager--box" style={{ width: height }}>
+          {counter}
+        </div>
+        <div className="project-pager--box" style={{ width: height }}>
+          <Link to={`/${category}/${project.nextProject}`}>NEXT</Link>
+        </div>
+      </>
+    )}
     <Selected height={height} />
   </div>
 );
