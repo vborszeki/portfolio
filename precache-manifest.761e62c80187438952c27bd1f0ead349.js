1c18a0b2fc4f94b35b38fb2df80cecc7b94b5e33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-24 12:53:39 +0000

    Deploy bntms/portfolio to github.com/bntms/portfolio.git:gh-pages

---
+++ precache-manifest.761e62c80187438952c27bd1f0ead349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331fd702bf3afea804448e3d73693da9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2317a18d375ee346a27e",
+    "url": "/portfolio/static/css/main.1ef58e55.chunk.css"
+  },
+  {
+    "revision": "58f5ea103fe86d26847e",
+    "url": "/portfolio/static/js/2.06965196.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.06965196.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2317a18d375ee346a27e",
+    "url": "/portfolio/static/js/main.a44b5943.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
